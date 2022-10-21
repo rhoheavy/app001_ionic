d1c0ba7666ca7a0b54c5162495d8a7da005f20d5
Rena Salazar <renasalazart@outlook.com>
2022-10-21 13:22:17 -0600

    Cards

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { CardsPageRoutingModule } from './cards-routing.module';
-
 import { CardsPage } from './cards.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CardsPageRoutingModule
+    CardsPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [CardsPage]
 })
